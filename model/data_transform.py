54767c4f1fd9eee30bda440d147975c5c69e863a
soon <dyd3312@gmail.com>
2020-03-30 10:01:44 +0900

    data_transform

--- model/data_transform.py
+++ model/data_transform.py
@@ -27,10 +27,84 @@ class Pipeline:
         ys = np.fromstring(open(os.path.join("/data/origin_label", idx), "rb").read(), dtype=np.uint8)
         return imgs.reshape(-1, self.height, self.width, 3), ys.reshape(-1, self.height // 2, self.width // 2)
 
+    def bw_global(self, img_gray):
+        i = random.randint(0, 2) # global binarization
+        thresh = 100 + 28 * i
+        ret, img_bw = cv2.threshold(img_gray, thresh, 255, cv2.THRESH_BINARY)
+        return img_bw
+
+    def bw_adaptive(self, img_gray, bw_type="Gaussian"):
+        block_size = 11
+
+        C = 3 + 2 * random.randint(0, 3)
+        if bw_type == "Gaussian":
+            img_bw = cv2.adaptiveThreshold(img_gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, block_size, C)
+        elif bw_type == "Mean":
+            img_bw = cv2.adaptiveThreshold(img_gray, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, block_size, C)
+        else:
+            raise
+        img_bw = cv2.cvtColor(img_bw, cv2.COLOR_GRAY2RGB).astype(np.uint8)
+        return img_bw
+
+    def saturation(self, img):
+        img_hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
+        orgH, orgS, orgV = cv2.split(img_hsv)
+
+        orgS = orgS.astype("float32")
+        factor = 0.25 * (1 + random.randint(0, 6))
+        s = np.clip(orgS * factor, 0, 255).astype(np.uint8)
+
+        img_hsv = cv2.merge([orgH, s, orgV])
+        img_saturated = cv2.cvtColor(img_hsv, cv2.COLOR_HSV2RGB).astype(np.uint8)
+        return img_saturated
+
+    def contrast_brightness(self, img):
+        const_start = 0.25
+        const_end = 2.0
+        contrastStep = 0.25
+        brightnessStep = 25
+
+        contrastRange = np.arange(const_start, const_end + contrastStep, contrastStep)
+        c = contrastRange[np.random.randint(len(contrastRange))]
+
+        brightness_start = max(-100, -100 * (c - 0.25))
+        brightness_end = min(100, 100 * (1.75 - c))
+        brightnessRange = np.arange(brightness_start, brightness_end + brightnessStep, brightnessStep)
+        b = brightnessRange[np.random.randint(len(brightnessRange))]
+        img = np.clip(img.astype(np.float32) * c + b, 0, 255).astype(np.uint8)
+        return img
+
+    def blur(self, img):
+        sigma = np.random.randint(2, 6)
+        img_blur = cv2.GaussianBlur(img, (0, 0), sigma)
+        return img_blur
+
+    def noise_generate(self, img):
+        noise_type = ["BW", "Saturation", "ContrastBrightness", "Blur"]
+        np.random.shuffle(noise_type)
+        shuffle_num = np.random.randint(1, 4)
+        for noise in noise_type[:shuffle_num]:
+            if noise == "BW":
+                img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+                bw_type_list = ["Gaussian", "Mean"]
+                img = self.bw_adaptive(img, bw_type_list[shuffle_num % 2])
+            elif noise == "Saturation":
+                img = self.saturation(img)
+            elif noise == "ContrastBrightness":
+                img = self.contrast_brightness(img)
+            elif noise == "Blur":
+                img = self.blur(img)
+            else:
+                raise
+        return img
+
     def transform(self, imgs, ys):
         assert imgs.shape[0] == ys.shape[0]
         batch = imgs.shape[0]
-        for b in tqdm.tqdm(range(batch)):
+        for b in range(batch):
+
+            imgs[b] = self.noise_generate(imgs[b].copy())
+
             if b % 2 == 0:
                 curve_random = random.randint(5, 20)
                 curve_mode = "down" if curve_random > 10 else "up"
@@ -48,7 +122,7 @@ class Pipeline:
 
         #heatmap_img = cv2.applyColorMap(ys[0], cv2.COLORMAP_JET)
         #cv2.imwrite("label_curved.png", heatmap_img)
-        #cv2.iimwrite("origin_curved.png", imgs[0])
+        #cv2.imwrite("origin_curved.png", imgs[0])
         return imgs, ys
 
     def save(self, imgs, ys, idx=0):
@@ -64,10 +138,12 @@ class Pipeline:
             fout.write(ys.tostring())
 
     def run(self):
-        for batch in self.load_files():
+        for batch in tqdm.tqdm(self.load_files()):
             imgs, ys = self.load_np(batch)
-            print(batch, imgs.shape, ys.shape)
             imgs, ys = self.transform(imgs, ys)
+            print("res", imgs.shape, ys.shape)
+            print("res", imgs.dtype, ys.dtype)
+            break
             self.save(imgs, ys, batch)
 
 
