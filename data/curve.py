efd4a4f0cbfe0152fcae36bd0a230f984841983c
soon <dyd3312@gmail.com>
2020-05-04 19:11:19 +0900

    curve and folded

--- data/curve.py
+++ data/curve.py
@@ -1,15 +1,39 @@
 import fire
 
-from base_transform import Transform
+from base_transform import Shift
 
 
-class Curve(Transform):
+class Curve(Shift):
+    """
+    문서 말림 noise
+
+    **How to use??**
+
+    1. 가로 말림
+    (약)
+    $> python curve.py --width=960 --height=1280 --channel=3 --ipt="sample.png" is_horizon=True flexure=0.1
+    (강)
+    $> python curve.py --width=960 --height=1280 --channel=3 --ipt="sample.png" is_horizon=True flexure=0.2
+
+    2. 세로 말림
+    (약)
+    $> python curve.py --width=960 --height=1280 --channel=3 --ipt="sample.png" is_horizon=False flexure=0.1
+    (강)
+    $> python curve.py --width=960 --height=1280 --channel=3 --ipt="sample.png" is_horizon=False flexure=0.2
+
+    """
 
     def __init__(self, flexure=0.1, direction="down", **kwargs):
         """
         cureve origin paper to curved paper
+        :param width: 문서 너비(pixel)
+        :param height: 문서 높이(pixel)
+        :param spacing: shift 하는 문서 부분 크기, 클수록 빠르게 변환하지만
+            정확도가 떨어진다. default: 20
+        :param is_horizon: 가로 말림이면 True, 세로 말림이면 False
+        :param verbose: 디버깅용이면 True
         :param flexure: maximum shifted pixel within ratio
-        :param direction: curve direction
+        :param direction: curve direction, "down" or "up"
         """
         super().__init__(**kwargs)
 
@@ -26,4 +50,6 @@ class Curve(Transform):
 
 
 if __name__ == "__main__":
-    fire.Fire(Curve)
+    curve = Curve(width=960, height=1280, direction="up", is_horizon=False, verbose=True)
+    curve.run(3, ipt="report.png")
+    #fire.Fire(Curve)
--- data/folded.py
+++ data/folded.py
@@ -1,11 +1,42 @@
 import fire
 
-from base_transform import Transform
+from base_transform import Shift
 
 
-class Folded(Transform):
+class Folded(Shift):
+    """
+    문서 접힘 noise
 
+    **How to use??**
+
+    1. 가로 접힘
+    (약)
+    $> python curve.py --width=960 --height=1280 --channel=3 --ipt="sample.png"
+        --is_horizon=True --up_slope=0.1 --down_slope=0.1
+    (강)
+    $> python curve.py --width=960 --height=1280 --channel=3 --ipt="sample.png"
+        --is_horizon=True --up_slope=0.2 --down_slope=0.2
+
+    2. 세로 접힘
+    (약)
+    $> python curve.py --width=960 --height=1280 --channel=3 --ipt="sample.png"
+        --is_horizon=False --up_slope=0.1 --down_slope=0.1
+    (강)
+    $> python curve.py --width=960 --height=1280 --channel=3 --ipt="sample.png"
+        --is_horizon=False --up_slope=0.2 --down_slope=0.2
+    """
     def __init__(self, up_slope=0.1, down_slope=0.2, **kwargs):
+        """
+        cureve origin paper to curved paper
+        :param width: 문서 너비(pixel)
+        :param height: 문서 높이(pixel)
+        :param spacing: shift 하는 문서 부분 크기, 클수록 빠르게 변환하지만
+            정확도가 떨어진다. default: 20
+        :param is_horizon: 가로 접힘이면 True, 세로 접힘이면 False
+        :param verbose: 디버깅용이면 True
+        :param up_slope: upper slide amount within ratio
+        :param down_slope: below slide amount within ratio
+        """
         super().__init__(**kwargs)
 
         self.up_slope = up_slope
