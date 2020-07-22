9adbdd3d06162f8744aa81989b0273edce6f8a94
soon <dyd3312@gmail.com>
2020-07-22 12:04:11 +0900

    transform module

---
+++ data/transform_api.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+
+def transformImage(image, coords, transformType, option):
+    if coords is None:
+        transCoords = None
+    transImage = None
+    return transImage, transCoords
