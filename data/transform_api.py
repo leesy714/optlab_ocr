a9037491aca401189b70019967ed22dfb3545fe5
soon <dyd3312@gmail.com>
2020-07-22 12:39:49 +0900

    structure

--- data/transform_api.py
+++ data/transform_api.py
@@ -1,8 +1,56 @@
+import abc
 import numpy as np
 
 
+class Transform(metaclass=abc.ABCMeta):
+
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    @abstractmethod
+    def transform_image(self, image):
+        pass
+
+    @abstractmethod
+    def transform_points(self, points):
+        pass
+
+class Shift(Transform):
+    pass
+
+class Folded(Shift):
+    pass
+
+class Curve(Shift):
+    pass
+
+
 def transformImage(image, coords, transformType, option):
-    if coords is None:
-        transCoords = None
-    transImage = None
+
+    height, width, channel = image.shape
+    tran = None
+
+    if transformType is "1A":
+        raise NotImplementedError
+
+    elif transformType is "1B":
+        raise NotImplementedError
+
+    elif transformType is "1C":
+        tran = Folded(width=width, height=height, spacing=40)
+
+    elif transformType is "1D":
+        raise NotImplementedError
+
+    elif transformType is "2A":
+        raise NotImplementedError
+
+    elif transformType is "2B":
+        raise NotImplementedError
+
+    assert isinstance(tran, Transform)
+    transImage = tran.run(image)
+    transCoords = tran.transform_points(coords)
+
     return transImage, transCoords
