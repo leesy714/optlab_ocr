a2718767fd0c8c6ec44cb6c5667752d1d5c8dd51
gyeomski <rlackdrua25@snu.ac.kr>
2020-07-27 17:26:05 +0900

    add perspective to transform_api 2

--- data/transform_api.py
+++ data/transform_api.py
@@ -63,6 +63,7 @@ class Shift(Transform):
                             [width + self.spacing + self.slide(h+1), +height + self.spacing],
                             [width + self.slide(h+1), height + self.spacing]], np.float32)
             mat = cv2.getPerspectiveTransform(src, dst)
+            
             point = np.array((*point, 1))
             t_point = np.dot(mat, point)
             t_points.append((int(t_point[0]), int(t_point[1])))
@@ -206,8 +207,6 @@ class Perspective(Transform):
         pts1 = np.float32([[0,0],[self.width,0],[0,self.height],[self.width,self.height]])
         
         option, degree = options.split('-')[0], options.split('-')[1]
-        print(option)
-        print(degree)
         
         x = int(self.width*(1-self.width_ratio)/2)
         y = int(self.height*(1-self.height_ratio)/2)
@@ -255,22 +254,24 @@ class Perspective(Transform):
         
 
     def transform_points(self, points):
-        
+        points = points.reshape(-1, 2)
+        points = [(x[0], x[1]) for x in points]
         t_points = []
         for point in points:
             point = np.array((*point, 1))
             t_point = np.dot(self.mat, point)
             t_points.append((int(t_point[0]), int(t_point[1])))
-
+        t_points = np.array(t_points)
+        t_points = t_points.reshape(-1, 4, 2)
         return t_points
     
     # transform_image
     def transform_image(self, ipt, channel=3, ipt_format="file", opt="res_pers.png", opt_format="file", base="Arles-15t.jpg", base_format="file"):
         
-        self.origin = cv2.resize(ipt, dsize=(self.width, self.height))
+        origin = cv2.resize(ipt, dsize=(self.width, self.height))
         if channel > 1:
-            self.origin = cv2.cvtColor(self.origin, cv2.COLOR_BGR2BGRA)
-        self.origin = cv2.resize(self.origin, dsize=(self.width, self.height))
+            origin = cv2.cvtColor(origin, cv2.COLOR_BGR2BGRA)
+        origin = cv2.resize(origin, dsize=(self.width, self.height))
 
         # 배경 이미지 load
         # TODO: temporal black base image
@@ -285,25 +286,23 @@ class Perspective(Transform):
         
         # 이미지 변형
         
-        print(self.origin.shape)
-        self.origin = cv2.warpPerspective(self.origin, self.mat, (self.width,self.height), flags = cv2.INTER_CUBIC, borderMode=cv2.BORDER_CONSTANT, borderValue = [0, 0, 0, 0])
-        print(self.origin.shape)
+        origin = cv2.warpPerspective(origin, self.mat, (self.width,self.height), flags = cv2.INTER_CUBIC, borderMode=cv2.BORDER_CONSTANT, borderValue = [0, 0, 0, 0])
         
         # 배경과 합치기
         x1, x2 = 0, self.width
         y1, y2 = 0, self.height
         
         if channel > 1:
-            alpha_p = self.origin[:, :, 3] / 255.0
+            alpha_p = origin[:, :, 3] / 255.0
         else:
-            alpha_p = self.origin[:, :] / 255.0
+            alpha_p = origin[:, :] / 255.0
         alpha_b = 1.0 - alpha_p
 
         if channel > 1:
             for c in range(0, 3):
-                imgBase[y1:y2, x1:x2, c] = (alpha_p[y1:y2, x1:x2] * self.origin[y1:y2, x1:x2, c] + alpha_b[y1:y2, x1:x2] * imgBase[y1:y2, x1:x2, c])
+                imgBase[y1:y2, x1:x2, c] = (alpha_p[y1:y2, x1:x2] * origin[y1:y2, x1:x2, c] + alpha_b[y1:y2, x1:x2] * imgBase[y1:y2, x1:x2, c])
         else:
-            imgBase[y1:y2, x1:x2] = self.origin[y1:y2, x1:x2] + imgBase[y1:y2, x1:x2]
+            imgBase[y1:y2, x1:x2] = origin[y1:y2, x1:x2] + imgBase[y1:y2, x1:x2]
         
         #cv2.imwrite(opt, imgBase)
         return imgBase
