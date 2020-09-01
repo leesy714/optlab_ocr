89e0a41a08327b0fa54322fd75db88e0f49244f5
soon <dyd3312@gmail.com>
2020-09-01 15:36:18 +0900

    inference

--- model/inference.py
+++ model/inference.py
@@ -30,6 +30,40 @@ def accuracy_box(y_pred, box):
     #acc.append(value != 0)
     return value == label
 
+def IoU(pred, y):
+    argmax = np.argmax(pred, axis=2)
+    y = np.expand_dims(y, axis=2)
+    y_contours, hierachy = cv2.findContours(y, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
+    if len(y_contours) == 2:
+        y_contours = y_contours[0]
+    elif len(y_contours) == 3:
+        y_contours = y_contours[1]
+
+    argmax[argmax != 0] = 100
+    argmax = np.expand_dims(argmax, axis=2).astype(np.float32)
+    argmax = cv2.GaussianBlur(argmax, (5, 5), 0)
+    argmax[argmax > 30] = 100
+    argmax[argmax <= 30] = 0
+    argmax = argmax.astype(np.uint8)
+
+    pred_contours, hierachy = cv2.findContours(argmax, cv2.RETR_CCOMP, cv2.CHAIN_APPROX_NONE)
+    if len(pred_contours) == 2:
+        pred_contours = pred_contours[0]
+    elif len(pred_contours) == 3:
+        pred_contours = pred_contours[1]
+
+    blank1 = np.zeros( (640, 480) )
+    blank2 = np.zeros( (640, 480) )
+    for contour in y_contours:
+        if cv2.contourArea(contour) > 5:
+            blank1 = cv2.drawContours(blank1, [contour], -1, 1, -1)
+    for contour in pred_contours:
+        if cv2.contourArea(contour) > 5:
+            blank2 = cv2.drawContours(blank2, [contour], -1, 1, -1)
+    intersection = np.sum((blank1 + blank2) == 2)
+    union = np.sum((blank1 + blank2) > 0)
+    return intersection / (union + 1e-9)
+
 
 class RealDataTest(Data0001):
     CANVAS_SIZE = 1280
@@ -126,6 +160,7 @@ def test(classes=None):
     #accs, recs, pres, acc_boxs = 0.0, 0.0, 0.0, 0.0
     recs_label, pres_label = [], []
     recs_group, pres_group = [], []
+    IoUs_label, IoUs_group = [], []
 
     for imgs, crafts, ys, groups, boxing, idx in test_loader:
         file_num = int(idx[0])
@@ -146,8 +181,9 @@ def test(classes=None):
         rec_label, pre_label = float(rec_label), float(pre_label)
         recs_label.append(rec_label)
         pres_label.append(pre_label)
-        print(rec_label, pre_label)
         f1_label = 2*rec_label*pre_label / (rec_label + pre_label + 1e-8)
+        IoU_label = IoU(pred_label.squeeze().cpu().data.numpy(), ys.squeeze().cpu().data.numpy())
+        IoUs_label.append(IoU_label)
 
         _, argmax_group = pred_group.max(dim=3)
         rec_group = recall(groups.view(-1, 1), argmax_group.view(-1, 1))
@@ -156,9 +192,11 @@ def test(classes=None):
         recs_group.append(rec_group)
         pres_group.append(pre_group)
         f1_group = 2*rec_group*pre_group / (rec_group + pre_group + 1e-8)
+        IoU_group = IoU(pred_group.squeeze().cpu().data.numpy(), groups.squeeze().cpu().data.numpy())
+        IoUs_group.append(IoU_group)
 
-        print("[file-{:06d}] label f1-score: {:.4}, group f1-score: {:.4}".format(
-            file_num, float(f1_label), float(f1_group)))
+        print("[file-{:04d}] label f1-score: {:.4}, group f1-score: {:.4}, IoUs - l {:.4}, g {:.4}".format(
+            file_num, float(f1_label), float(f1_group), float(IoU_label), float(IoU_group)))
 
         imgs = imgs.permute(0, 2, 3, 1)
         imgs = imgs.squeeze().cpu().data.numpy()
@@ -167,11 +205,15 @@ def test(classes=None):
         imgs = np.clip(imgs, 0, 255).astype(np.uint8)
         craft = crafts.squeeze().cpu().data.numpy()
         ys = ys.squeeze().cpu().data.numpy()
-        #pred = pred.squeeze().cpu().data.numpy()
+        groups = groups.squeeze().cpu().data.numpy()
+        pred_label = pred_label.squeeze().cpu().data.numpy()
+        pred_group = pred_group.squeeze().cpu().data.numpy()
 
-        #np.save("./res/model/{:06d}_craft.npy".format(file_num), craft)
-        #np.save("./res/model/{:06d}_true.npy".format(file_num),  ys)
-        #np.save("./res/model/{:06d}_pred.npy".format(file_num), pred)
+        np.save("./res/model/{:04d}_craft.npy".format(file_num), craft)
+        np.save("./res/model/{:04d}_true_label.npy".format(file_num),  ys)
+        np.save("./res/model/{:04d}_pred_label.npy".format(file_num), pred_label)
+        np.save("./res/model/{:04d}_true_group.npy".format(file_num), groups)
+        np.save("./res/model/{:04d}_pred_group.npy".format(file_num), pred_group)
         #np.save("./res/model/{:06d}_pred.npy".format(file_num),  argmax)
 
         argmax_label = argmax_label.cpu().data.numpy()
@@ -184,6 +226,9 @@ def test(classes=None):
         argmax_group = np.clip(argmax_group*(255 / 15), 0, 255).astype(np.uint8)
         argmax_group = cv2.applyColorMap(argmax_group, cv2.COLORMAP_JET)
         #argmax_img = draw_polygon(argmax_img, argmax, bbox)
+
+        cv2.putText(argmax_label, "F1_SCORE: {:.4}".format(f1_label), (5, 640 - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
+        cv2.putText(argmax_group, "F1_SCORE: {:.4}".format(f1_group), (5, 640 - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255))
         cv2.imwrite("./res/model/{:04d}_base.png".format(file_num), img)
         cv2.imwrite("./res/model/{:04d}_pred_label.png".format(file_num), argmax_label)
         cv2.imwrite("./res/model/{:04d}_pred_group.png".format(file_num), argmax_group)
